03c5bdc5182138aa01ea4f1ae903e43c06a468c6
Timtim <timkrow@centurylink.net>
2020-02-19 17:12:00 -0500

    Update hello-yoda.php

--- hello-yoda.php
+++ hello-yoda.php
@@ -61,7 +61,7 @@ function hello_yoda_menu() {
 
 function hello_yoda_add_quote_page(){
 	echo '	<h1>Add A Quote</h1>
-		  	<form class="add-quote" method="post">
+		  	<form action="hello-yoda-add-quote" method="post">
 				<p>Quote</p>
 				<input name="quote" id="quote" type="text"><br /><br /><br />
 				<p>Quotee</p>
