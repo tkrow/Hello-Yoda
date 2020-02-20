12e01afb7f4048217d9f01d7badf2b6092f32021
Timtim <timkrow@centurylink.net>
2020-02-20 11:30:59 -0500

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,8 @@
+<?php
 if (!defined('WP_UNINSTALL_PLUGIN')){
     die;
 }
 
 global $wpdb;
-	$wpdb->query("DROP TABLE {$wpdb->prefix}hello_yoda_quotes");
+$wpdb->query("DROP TABLE {$wpdb->prefix}hello_yoda_quotes");
+?>
